8ee95f036e72323e823db3a5c4f671245859b2ad
Tyler Wong <tywong8@gmail.com>
2023-06-15 20:41:31 -0400

    transitions to proper scenes

--- 2023SummerGameJam/Assets/Scripts/UIManager.cs
+++ 2023SummerGameJam/Assets/Scripts/UIManager.cs
@@ -2,28 +2,37 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using DG.Tweening;
 
 public class UIManager : MonoBehaviour
 {
-    [SerializeField] private Button startButton;
+    [SerializeField] List<GameObject> buttons = new List<GameObject>();  
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        ButtonAnim();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+       
     }
 
-    private void Awake()
+    public void QuitGame()
     {
-        startButton.onClick.AddListener(() =>
-        {
+        Application.Quit();
+    }
 
-        }); 
+    public void ButtonAnim()
+    {
+        Sequence sequence = DOTween.Sequence();
+
+        sequence
+            .Append(buttons[0].transform.DOMoveX(1003, 1).SetEase(Ease.InOutExpo))
+            .Append(buttons[1].transform.DOMoveX(1003, 1).SetEase(Ease.InOutExpo))
+            .Append(buttons[2].transform.DOMoveX(1003, 1).SetEase(Ease.InOutExpo))
+            .Append(buttons[3].transform.DOMoveX(1003, 1).SetEase(Ease.InOutExpo));     
     }
 }
