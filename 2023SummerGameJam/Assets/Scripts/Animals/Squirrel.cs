cddf55b85c6148bbedf23f3ba84a0b2c4230b86a
Ramzi Dajani <ramzi.r.dajani@gmail.com>
2023-06-16 11:26:25 -0400

    Added Wall Climb for squirrel

--- 2023SummerGameJam/Assets/Scripts/Animals/Squirrel.cs
+++ 2023SummerGameJam/Assets/Scripts/Animals/Squirrel.cs
@@ -9,5 +9,39 @@ public class Squirrel : Animal
         base.Update();
 
         characterController.SimpleMove(moveDir * moveSpeed);
+
+        Shoot();
+
+        if (Input.GetKey("w") & TouchingWall == true)
+        {
+            transform.position += Vector3.up * Time.deltaTime * UpwardSpeed;
+            TouchingWall = false;
+        }
+
+        if (Input.GetKeyUp("w"))
+        {
+            TouchingWall = false;
+        }
+    }
+
+    public float open = 100f;
+    public float range = 1f;
+    public bool TouchingWall = false;
+    public float UpwardSpeed = 3.3f;
+    public Transform Head;
+
+    void Shoot()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(Head.position, Head.forward, out hit, range))
+        {
+            Debug.Log(hit.transform.name);
+
+            Collider target = hit.transform.GetComponent<Collider>();
+            if (target != null)
+            {
+                TouchingWall = true;
+            }
+        }
     }
 }
