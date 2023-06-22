f224a1c1d0d6ded7747b06cb754abbbc014e8b5e
Ramzi Dajani <ramzi.r.dajani@gmail.com>
2023-06-22 00:52:48 -0400

    Alerts can now chain

--- 2023SummerGameJam/Assets/Scripts/AlertSystem.cs
+++ 2023SummerGameJam/Assets/Scripts/AlertSystem.cs
@@ -1,8 +1,8 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
 using TMPro;
+using System.Linq;
 
 public class AlertSystem : MonoBehaviour
 {
@@ -13,6 +13,8 @@ public class AlertSystem : MonoBehaviour
     public float waitTime = 2.0f;
     [SerializeField] GameObject AlertBox;
     [SerializeField] TextMeshProUGUI AlertText;
+
+    List<string> messageQueue = new List<string>();
 
     public static AlertSystem Instance { get; private set; }
 
@@ -29,13 +31,32 @@ public class AlertSystem : MonoBehaviour
         }
     }
 
+    private void Update()
+    {
+        if (alertTween == null && messageQueue.Count > 0)
+        {
+            alertTween = DOTween.Sequence();
+
+            AlertText.SetText(messageQueue[0]);
+            messageQueue.RemoveAt(0);
+
+            alertTween
+                .Append(AlertBox.transform.DOScale(new Vector3(1f, 1f, 1f), panelTweenTime).SetEase(Ease.InOutExpo))
+                .AppendInterval(waitTime)
+                .Append(AlertBox.transform.DOScale(new Vector3(0f, 0f, 0f), panelTweenTime).SetEase(Ease.InOutExpo))
+                .SetRecyclable(true)
+                .SetAutoKill(false);
+        }
+        if (messageQueue.Count > 0 && !alertTween.IsPlaying())
+        {
+            AlertText.SetText(messageQueue[0]);
+            messageQueue.RemoveAt(0);
+            alertTween.Restart();
+        }
+    }
+
     public void CreateAlert(string message)
     {
-        alertTween = DOTween.Sequence();
-        AlertText.SetText(message);
-        alertTween.Append(AlertBox.transform.DOScale(new Vector3(1f, 1f, 1f), panelTweenTime).SetEase(Ease.InOutExpo))
-        .AppendInterval(waitTime)
-        .Append(AlertBox.transform.DOScale(new Vector3(0f, 0f, 0f), panelTweenTime).SetEase(Ease.InOutExpo));
-
+        messageQueue.Add(message);
     }
 }
