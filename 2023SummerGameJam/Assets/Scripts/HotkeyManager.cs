4caf67eedc4d54d9734adc905e0a19d6c1f913a0
Ramzi Dajani <ramzi.r.dajani@gmail.com>
2023-06-29 20:08:35 -0400

    Added number of each animal to hotkey

--- 2023SummerGameJam/Assets/Scripts/HotkeyManager.cs
+++ 2023SummerGameJam/Assets/Scripts/HotkeyManager.cs
@@ -2,10 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using DG.Tweening;
+using TMPro;
 
 public class HotkeyManager : MonoBehaviour
 {
     public GameObject[] HotkeyIcons;
+    [SerializeField] bool[] active = new bool[4];
 
     public static HotkeyManager Instance { get; private set; }
 
@@ -20,20 +22,41 @@ public class HotkeyManager : MonoBehaviour
             Instance = this;
         }
 
+        active[0] = true;
         for (int i = 1; i < 4; i++)
         {
             HotkeyIcons[i].transform.localScale = new Vector3(0f, 0f, 0f);
         }
     }
 
+    private void Update()
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            if (active[i])
+            {
+                int numAnimals = CommunityManager.Instance.animalsInCommunity[i].Count;
+                HotkeyIcons[i].transform.GetChild(2).GetComponent<TextMeshProUGUI>().SetText($"x{numAnimals}");
+            }
+        }
+    }
+
     public void expandIcon(int index)
     {
-        HotkeyIcons[index].transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f).SetEase(Ease.InOutExpo);
+        if (!active[index])
+        {
+            HotkeyIcons[index].transform.DOScale(new Vector3(1f, 1f, 1f), 0.5f).SetEase(Ease.InOutExpo);
+            active[index] = true;
+        }
     }
 
     public void collapseIcon(int index)
     {
-        HotkeyIcons[index].transform.DOScale(new Vector3(0f, 0f, 0f), 0.5f).SetEase(Ease.InOutExpo);
+        if (active[index])
+        {
+            HotkeyIcons[index].transform.DOScale(new Vector3(0f, 0f, 0f), 0.5f).SetEase(Ease.InOutExpo);
+            active[index] = false;
+        }
     }
 
 }
